31e3b2762c6f95d7543e92f9ec5aa67c66e0b390
MatheoDarlo <Matheo.Darlo@ynov.com>
2025-12-10 15:48:14 +0100

    ajt couleur verte

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
-
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "route: %q", r.URL.Path)
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+fmt.Fprintf(w, "<h1Colors</h1><style>*{background-color: #006400;}
+</style>")
 }
 
 func main() {
-	http.HandleFunc("/", IndexHandler)
-	// to add : color functionality
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/color", ColorHandler)
 }
